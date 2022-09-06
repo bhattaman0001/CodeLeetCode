3131cb80545a92a08d85c57c5f1ad421f1a381fb
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-06 17:09:10 +0530

    Time: 19 ms (7.97%), Space: 7.7 MB (25.60%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,22 +1,58 @@
-class Solution {
-public:    
-    bool isPalindrome(string s) {
-        if(s.size() == 0) return 1;
-        string temp;
-        for(int i=0;i<s.size();i++){
-            if((s[i] >= '0' and s[i] <= '9') || (s[i] >= 'a' and s[i] <= 'z')){
-                temp.push_back(s[i]);
+class Solution
+{
+    public:
+        bool isPalindrome(string s)
+        {
+            if (s.size() == 0) return 1;
+           	        string temp;
+           	        for(int i=0;i < s.size();i++){
+           	            if((s[i] >= '0' and s[i] <= '9') || (s[i] >= 'a' and s[i] <= 'z')){
+           	                temp.push_back(s[i]);
+           	            }
+           	            else if(s[i] >= 'A' and s[i] <= 'Z'){
+           	                temp.push_back(tolower(s[i]));
+           	            }
+           	        }
+            
+            int i=0,j=temp.size()-1;
+            while(i < j){
+                if(temp[i] != temp[j]) return 0;
+                i++;j--;                
             }
-            else if(s[i] >= 'A' and s[i] <= 'Z'){
-                temp.push_back(tolower(s[i]));
-            }
+            return 1;
+            
+
+            // for (int i = 0; i < s.size(); i++)
+            // {
+            //     if ((s[i] >= '0'
+            //             and s[i] <= '9') || (s[i] >= 'a'
+            //             and s[i] <= 'z'))
+            //     {
+            //         continue;
+            //     }
+            //     else if (s[i] >= 'A'
+            //         and s[i] <= 'Z')
+            //     {
+            //         s[i] = tolower(s[i]);
+            //     }
+            //     else
+            //     {
+            //         s[i] = '#';
+            //     }
+            // }
+            // cout<<s<<"\n";
+            // int i = 0, j = s.size() - 1;
+            // while (i < j)
+            // {
+            //     if (s[i] == '#') i++;
+            //     if (s[j] == '#') j--;
+            //     else if (s[i] != s[j]) return 0;
+            //     else
+            //     {
+            //         i++;
+            //         j--;
+            //     }
+            // }
+            // return 1;
         }
-        
-        int i=0,j=temp.size()-1;
-        while(i < j){
-            if(temp[i] != temp[j]) return 0;
-            i++;j--;
-        }
-        return 1;
-    }
 };
