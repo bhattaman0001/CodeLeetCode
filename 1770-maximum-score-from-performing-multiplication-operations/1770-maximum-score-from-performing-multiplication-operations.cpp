88a956b76b9dd676723154ccafd3b5fb8a9da17f
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-16 16:36:57 +0530

    Time: 465 ms (86.25%), Space: 119.9 MB (68.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    
     int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
-        
         if (j == M.size()) return 0;
         if (dp[i][j] != INT_MIN) return dp[i][j];
-        
-        // Left Side
-        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
-        
-        // Right Side
-        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
-        
+        int e = n - (j-i) - 1;
+        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);        
+        int right = solve(i, n, j + 1, nums, M) + (nums[e] * M[j]);
         return dp[i][j] = max(left, right);
-    }
-    
+    }    
     int maximumScore(vector<int>& nums, vector<int>& M) {   
         int n = nums.size(), m = M.size();
         dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
