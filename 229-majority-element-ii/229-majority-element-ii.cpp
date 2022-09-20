4c18c313902f4b3595cfca2795d564994ee14e19
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-20 18:05:26 +0530

    Time: 16 ms (80.39%), Space: 15.9 MB (56.84%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -4,12 +4,8 @@ public:
         unordered_map<int, int> mp;
         int n = nums.size();
         vector<int> v;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;            
-        }
-        for(auto x : mp){
-            if(x.second > n/3) v.push_back(x.first);
-        }
+        for(int i=0;i<n;i++) mp[nums[i]]++;            
+        for(auto x : mp) if(x.second > n/3) v.push_back(x.first);        
         return v;
     }
 };
