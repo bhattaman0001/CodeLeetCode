b41edd8d6acba5c4306935b79a66f132970024e9
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-21 11:33:40 +0530

    Time: 121 ms (86.71%), Space: 46.1 MB (38.69%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -5,27 +5,11 @@ public:
         int n = queries.size();
         int evenSum = 0;
         for(int i=0;i<nums.size();i++) if(nums[i]%2==0) evenSum += nums[i];        
-        for(int i=0;i<n;i++){
-            if(nums[queries[i][1]] % 2 == 0){
-                if(queries[i][0] % 2 == 0){
-                    nums[queries[i][1]] += queries[i][0];
-                    evenSum += queries[i][0];
-                    v.push_back(evenSum);
-                }else{
-                    evenSum -= nums[queries[i][1]];
-                    nums[queries[i][1]] += queries[i][0];
-                    v.push_back(evenSum);
-                }
-            }else{
-                 if(queries[i][0] % 2 != 0){
-                    nums[queries[i][1]] += queries[i][0];
-                    evenSum += nums[queries[i][1]];
-                    v.push_back(evenSum);
-                }else{      
-                    nums[queries[i][1]] += queries[i][0];
-                    v.push_back(evenSum);
-                }  
-            }            
+        for(auto &q : queries){
+            if(nums[q[1]] % 2 == 0) evenSum -= nums[q[1]];
+            nums[q[1]] += q[0];
+            if(nums[q[1]] % 2 == 0) evenSum += nums[q[1]];
+            v.push_back(evenSum);
         }
         return v;
     }
