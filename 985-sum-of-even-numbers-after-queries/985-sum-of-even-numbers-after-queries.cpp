202cf477aa107049b731ac8e2ce229c1eacd9d5d
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-21 11:29:11 +0530

    Time: 224 ms (32.74%), Space: 46 MB (66.47%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:    
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> v;
+        int n = queries.size();
+        int totalSum = 0, evenSum = 0;
+        for(int i=0;i<nums.size();i++){            
+            if(nums[i]%2==0) evenSum += nums[i];
+        }
+        for(int i=0;i<n;i++){
+            if(nums[queries[i][1]] % 2 == 0){
+                if(queries[i][0] % 2 == 0){
+                    nums[queries[i][1]] += queries[i][0];
+                    evenSum += queries[i][0];
+                    v.push_back(evenSum);
+                }else{
+                    evenSum -= nums[queries[i][1]];
+                    nums[queries[i][1]] += queries[i][0];
+                    v.push_back(evenSum);
+                }
+            }else{
+                 if(queries[i][0] % 2 != 0){
+                    nums[queries[i][1]] += queries[i][0];
+                    evenSum += nums[queries[i][1]];
+                    v.push_back(evenSum);
+                }else{      
+                    nums[queries[i][1]] += queries[i][0];
+                    v.push_back(evenSum);
+                }  
+            }            
+        }
+        return v;
+    }
+};
