9cfc489b0d8f71419959c584b5000de4f195f545
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-21 11:50:11 +0530

    Time: 26 ms (15.77%), Space: 8.2 MB (82.69%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.cpp
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        string ans="";
+        string ans;
         int count=0;
         int n = s.size();
         for(int i=n-1;i>=0;i--){
             if(s[i] == '-') continue;
-            if(s[i] >= 'a' and s[i] <= 'z'){
-                s[i] = toupper(s[i]);
-            }
+            if(s[i] >= 'a' and s[i] <= 'z') s[i] = toupper(s[i]);            
             ans.push_back(s[i]);
             count++;
             if(count==k){
                 count=0;
-                if(i>0){
-                    ans.push_back('-');
-                }
+                if(i>0) ans.push_back('-');                
             }
         }
-        if(ans.size()==0) return "";
-        if(ans[ans.size()-1]=='-')ans.pop_back();
+        if(ans.size() == 0) return "";
+        if(ans[ans.size()-1]=='-') ans.pop_back();
         reverse(ans.begin(), ans.end());
         return ans;
     }
