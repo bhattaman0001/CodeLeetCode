7d9ac9256d0e7939ccfbdb2ba261ba3d6e6e508c
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-09-29 16:05:05 +0530

    Time: 4 ms (57.91%), Space: 9.1 MB (30.52%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,6 +4,15 @@ public:
         for(int i=m, j=0; i<m+n, j<n; i++, j++){
             nums1[i] = nums2[j];
         }
-        sort(nums1.begin(), nums1.end());
+        // sort(nums1.begin(), nums1.end());
+        for(int i=0;i<n+m;i++){
+            for(int j=i+1;j<n+m;j++){
+                if(nums1[i] > nums1[j]){
+                    int t = nums1[i];
+                    nums1[i] = nums1[j];
+                    nums1[j] = t;
+                }
+            }
+        }
     }
 };
