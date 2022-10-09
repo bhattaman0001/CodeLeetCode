2e8c5a44c04d759f0a29c0e86287c25c296b09e9
Aman Bhatt <87806212+bhattaman0001@users.noreply.github.com>
2022-10-09 16:34:50 +0530

    Time: 92 ms (29.61%), Space: 38.9 MB (24.07%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,26 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include<bits/stdc++.h>
 class Solution {
 public:
-    vector<int> v;
-    void inorder(TreeNode* root){
-        if(!root) return;
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
-    }
+    // vector<int> v;
+    // void inorder(TreeNode* root){
+    //     if(!root) return;
+    //     inorder(root->left);
+    //     v.push_back(root->val);
+    //     inorder(root->right);
+    // }
+    // bool findTarget(TreeNode* root, int k) {
+    //     if(!root) return false;
+    //     if(root and !root->left and !root->right) return false;
+    //     inorder(root);
+    //     int i = 0, j = v.size() - 1;
+    //     while(i < j){
+    //         int t = v[i] + v[j];
+    //         if(t == k) return true;
+    //         if(t > k) j--;
+    //         else i++;
+    //     }
+    //     return false;
+    // }
+    
+    unordered_set<int> s;    
     bool findTarget(TreeNode* root, int k) {
         if(!root) return false;
-        if(root and !root->left and !root->right) return false;
-        inorder(root);
-        int i = 0, j = v.size() - 1;
-        while(i < j){
-            int t = v[i] + v[j];
-            if(t == k) return true;
-            if(t > k) j--;
-            else i++;
-        }
-        return false;
+        if(s.count(k - root->val)) return 1;
+        s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
+    
+    
+    
 };
